1bf2b7db2fa453f21fa8c4d42b09450bf497d1ee
ajay-dds <ajay.anand@dds.mil>
2022-07-07 16:41:06 -0700

    new script to crawl gutenberg for bible

---
+++ code/religious_texts/new_testament.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_bible():
+
+    bible_source = "https://www.gutenberg.org/files/10/10-h/10-h.htm"
+    r = requests.get(bible_source)
+    soup = BeautifulSoup(r.content, "html5lib")
+
+    verses = [i.contents[0] for i in soup.find_all("p")]
+    chapter_verse = [i.split(" ")[0] for i in verses]
+
+    for i, cv in enumerate(chapter_verse):
+        if len(cv.split(":")) < 2:
+            verses.pop(i)
+            chapter_verse.pop(i)
+    for i, cv in enumerate(chapter_verse):
+        if len(cv.split(":")) < 2:
+            verses.pop(i)
+            chapter_verse.pop(i)
+
+    chapter_num = [i.split(":")[0] for i in chapter_verse]
+    verse_num = [i.split(":")[1] for i in chapter_verse]
+
+    verses = [" ".join(i.split(" ")[1:]) for i in verses]
+
+    bible = pd.DataFrame(zip(chapter_num, verse_num, verses), columns=["chapter_num", "verse_num", "verse"])
+    bible.to_csv("../data/Christianity/Bible/New_Testament/newtestament.csv", index=False)
+    return bible
